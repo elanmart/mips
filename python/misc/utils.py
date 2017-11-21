baac3b8f57ceec96e1f4cce54ad57f1410d9495b
Marcin Elantkowski <marcin.elantkowski@gmail.com>
2017-11-21 16:47:54 +0100

    add utility to generate fasttext file

--- python/misc/utils.py
+++ python/misc/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy.sparse as sp
+import os
 
 
 def load_sift(fname, dtype=np.float32):
@@ -18,3 +20,19 @@ def save_sift(obj, fname, dtype=np.float32):
     ]).astype(dtype)
 
     obj.tofile(fname)
+
+
+def to_ft(X: sp.csr_matrix, Y: sp.csr_matrix, fname: str):
+    os.makedirs(os.path.dirname(fname), exist_ok=True)
+
+    with open(fname, 'w') as f:
+
+        for row_x, row_y in zip(X, Y):
+            xs, ys = row_x.indices, row_y.indices
+
+            labels = [f'__label__{y}' for y in ys]
+            feats  = [f'feature_{x}' for x in xs]
+
+            line = ' '.join(labels + feats)
+
+            print(line, file=f)
