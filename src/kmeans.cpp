d873a228eaed3d1ddd35a59067acf014895471a0
Adam <akrasuski1@gmail.com>
2017-11-28 21:39:32 +0100

    implement priority queue for kmeans

--- src/kmeans.cpp
+++ src/kmeans.cpp
@@ -12,6 +12,8 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <set>
+#include <queue>
 
 using namespace std;
 using layer_t = IndexHierarchicKmeans::layer_t;
@@ -47,51 +49,44 @@ static vector<layer_t> make_layers(const FloatMatrix& vectors, size_t L) {
 static vector<size_t> predict(const vector<layer_t>& layers, FloatMatrix& queries, size_t qnum,
         size_t opened_trees, const FloatMatrix& vectors, size_t k_needed = 1) {
 
-    vector<size_t> candidates;
+    // Priority queue of {layer, centroid number} indexed by IP.
+    priority_queue<pair<float, pair<size_t, size_t>>> queue;
+    // The last layer (i.e. original points)
+    vector<std::pair<float, size_t>> best_points;
+
     for (size_t i = 0; i < layers.back().cluster_num; i++) {
-        candidates.push_back(i);
+        float result = faiss::fvec_inner_product(
+                queries.row(qnum), 
+                layers.back().kr.centroids.row(i),
+                queries.vector_length);
+        queue.push({result, {layers.size() - 1, i}});
     }
 
-    for (size_t layer_id = layers.size() - 1; layer_id != (size_t)(-1); layer_id--) {
-        vector<std::pair<float, size_t>> best_centroids;
-        for (auto c: candidates) {
-            float result = faiss::fvec_inner_product(
-                    queries.row(qnum), 
-                    layers[layer_id].kr.centroids.row(c),
-                    queries.vector_length);
+    for (size_t i = 0; !queue.empty() && i < opened_trees; i++) {
+        auto best = queue.top().second;
+        queue.pop();
+        
+        size_t layer_id = best.first;
+        size_t cid = best.second;
 
-            best_centroids.push_back({result, c});
-        }
-
-        if (best_centroids.size() > opened_trees) {
-            nth_element(
-                    best_centroids.begin(), 
-                    best_centroids.begin() + opened_trees,
-                    best_centroids.end(),
-                    greater<std::pair<float, size_t>>());
-            best_centroids.resize(opened_trees);
-        }
-
-        candidates.clear();
-
-        for (auto val_cid: best_centroids) {
-            size_t cid = val_cid.second;
-            candidates.insert(candidates.end(), 
-                    layers[layer_id].centroid_children[cid].begin(),
-                    layers[layer_id].centroid_children[cid].end()
-            );
+        for (auto child: layers[layer_id].centroid_children[cid]) {
+            if (layer_id == 0) {
+                float result = faiss::fvec_inner_product(
+                        queries.row(qnum), 
+                        vectors.row(child),
+                        queries.vector_length);
+                best_points.push_back({result, child});
+            }
+            else {
+                float result = faiss::fvec_inner_product(
+                        queries.row(qnum), 
+                        layers[layer_id - 1].kr.centroids.row(child),
+                        queries.vector_length);
+                queue.push({result, {layer_id - 1, child}});
+            }
         }
     }
     // Last layer - find best match.
-    vector<std::pair<float, size_t>> best_points;
-    for (auto c: candidates) {
-        float result = faiss::fvec_inner_product(
-                queries.row(qnum), 
-                vectors.row(c),
-                queries.vector_length);
-
-        best_points.push_back({result, c});
-    }
     if (best_points.size() > k_needed) {
         nth_element(
                 best_points.begin(), 
